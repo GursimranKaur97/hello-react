fb9ad0342638212fe230dbae5787b6e8e1c417df
GursimranKaur97 <gursimrankaur1664@gmail.com>
2024-05-14 13:17:29 +0530

    added react elements

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
-const parent = React.createElement(
-    "div", {id: "parent"}, 
-    [React.createElement("div", {id: "child1"}, [
-         React.createElement("h1", {id: "heading"}, "I'm an h1 tag"),
-         React.createElement("h2", {}, "I'm an h2 tag")
-        ]
-    ),
-    React.createElement("div", {id: "child2"}, [
-         React.createElement("h1", {id: "heading"}, "I'm an h1 tag"),
-         React.createElement("h2", {}, "I'm an h2 tag")
-        ]
-    )
-    ]
+import React from 'react';
+import ReactDOM from "react-dom/client"
+
+// React Element
+// React.createElement => Object => HTMLElement(render) when we render this to DOM it becomes HTML elemant
+// const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+
+// JSX is not HTML in Javascript, it's an HTML like syntax, If we're wtiting jsx in multiple lines we need to wrap those in brackets
+// JSX => Babel transpiles it to React.createElement => ReactElement- JS Object => HTMLElement(render)
+// const jsxHeading = <h1 className='head' tabIndex="1">Namaste React using JSX</h1>
+
+// React Element
+// const heading = (
+// <h1 className='head' tabIndex="1">
+//     Namaste React using JSX
+// </h1>)
+
+
+const Title = () => (
+    <h1 className='head' tabIndex='5'>
+        Namaste React using JSX
+    </h1>
+)
+// React Component ->// 1) Class Based Component(OLD) 2)Functional Component(NEW)
+
+const number = 1000;
+// React Functional Component - > It's a normal javascript fxn that returns a jsx code or react elements
+// Component Composition (call one component inside another)
+const HeadingComponent = () => (
+    <div id='container'>
+        <h2>{number}</h2>
+        <Title />
+        <Title></Title>
+        {Title()}
+    <h1 className='heading'>Namaste React Functional Component</h1>
+    </div>
 )
 
-// const heading = React.createElement(
-//     "h1", 
-//     {id: "heading"}, // attributes of the object
-//      "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading); // Render react element
+root.render(<HeadingComponent />); //Render react component
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent); //ReactElement(Object) => HTML(Browser Understands)
+// const parent = React.createElement(
+//     "div", {id: "parent"}, 
+//     [React.createElement("div", {id: "child1"}, [
+//          React.createElement("h1", {id: "heading"}, "I'm an h1 tag"),
+//          React.createElement("h2", {}, "I'm an h2 tag")
+//         ]
+//     ),
+//     React.createElement("div", {id: "child2"}, [
+//          React.createElement("h1", {id: "heading"}, "I'm an h1 tag"),
+//          React.createElement("h2", {}, "I'm an h2 tag")
+//         ]
+//     )
+//     ]
+// )
+
+// // const heading = React.createElement(
+// //     "h1", 
+// //     {id: "heading"}, // attributes of the object
+// //      "Hello World from React!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);
+// root.render(parent); //ReactElement(Object) => HTML(Browser Understands)
---
+++ README.md
@@ -0,0 +1,29 @@
+# Namaste React
+
+npx // execute a package
+
+npx parcel index.html
+//Create a server for us and will host our app on http://localhost:1234
+
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Tree Shaking - removed unused code
+- Different dev and prod bundles
+    npx parcel index.html(for dev) // npm run start OR npm start
+    npx parcel bundle index.html (for prod) // npm run build
+- JSX- Javascript Syntax which is easier to create React elements
